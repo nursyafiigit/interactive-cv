c8ac55479f4c8b8c40dc94a66918028fbd64b680
nursyafiigit <muhammadnursyafii@students.amikom.ac.id>
2025-07-17 22:32:18 +0700

    ganti edu

--- frontend/src/components/EducationVue.vue
+++ frontend/src/components/EducationVue.vue
@@ -1,3 +1,4 @@
+
 <template>
   <section
     id="pendidikan"
@@ -11,14 +12,14 @@
 
       <!-- Deskripsi -->
       <div
-        class="condes max-w-3xl mx-auto text-white text-center mt-6 mb-12"
+        class="max-w-3xl mx-auto text-white text-center mt-6 mb-12"
         data-aos="fade-up"
         data-aos-delay="200"
         data-aos-duration="800"
       >
         <p class="desc text-white/90 leading-relaxed">
           Sebagai seorang IT Developer, perjalanan pendidikan saya telah memberikan dasar yang kuat
-          untuk memahami dan menguasai teknologi informasi serta pengembangan perangkat lunak
+          untuk memahami dan menguasai teknologi informasi serta pengembangan perangkat lunak...
         </p>
       </div>
 
@@ -33,65 +34,59 @@
         ></div>
 
         <!-- Loop riwayat pendidikan -->
-        <div v-for="(edu, index) in educationHistory" :key="edu.id" class="mb-10 w-full px-4">
+        <div
+          v-for="(edu, index) in educationHistory"
+          :key="edu.id"
+          class="mb-10 w-full px-4"
+        >
           <!-- Desktop: layout kiri-kanan -->
-          <div v-if="index % 2 === 0" class="hidden md:flex justify-between items-center">
+          <div
+            v-if="index % 2 === 0"
+            class="hidden md:flex justify-between items-center"
+          >
             <div class="w-1/2 pr-8 flex justify-end">
-              <a :href="edu.link" target="_blank" rel="noopener noreferrer">
-                <div class="card max-w-md" data-aos="fade-right" data-aos-duration="800">
-                  <img :src="edu.logo" class="w-16 h-16 object-contain" />
-                  <div class="text-right">
-                    <p
-                      class="text-sm sm:text-xs md:text-sm font-poppins font-semibold text-yellow-500 mb-1"
-                    >
-                      {{ edu.period }}
-                    </p>
-                    <h3
-                      class="text-xl sm:text-base md:text-lg font-bold font-poppins text-white mb-1"
-                    >
-                      {{ edu.institution }}
-                    </h3>
-                    <p class="text-white/80 font-poppins text-sm sm:text-xs md:text-sm">
-                      {{ edu.major }}
-                    </p>
-                  </div>
+              <a
+                :href="edu.link"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="card max-w-md"
+                data-aos="fade-right"
+              >
+                <img :src="edu.logo" class="w-16 h-16 object-contain" />
+                <div class="text-right">
+                  <p class="text-sm sm:text-xs md:text-sm font-poppins font-semibold text-yellow-500 mb-1">{{ edu.period }}</p>
+                  <h3 class="text-xl sm:text-lg md:text-2xl font-bold font-poppins text-white mb-1">{{ edu.institution }}</h3>
+                  <p class="text-white/80 font-poppins text-sm sm:text-xs md:text-sm">{{ edu.major }}</p>
                 </div>
               </a>
             </div>
             <div class="w-1/2 flex justify-start">
               <!-- Bulet hanya muncul di desktop -->
-              <div
-                class="w-4 h-4 bulet rounded-full z-10 hidden md:block"
-                data-aos="fade-in"
-                data-aos-duration="800"
-                data-aos-delay="500"
-              ></div>
+              <div class="w-4 h-4 bulet rounded-full z-10 hidden md:block" data-aos="fade-in" data-aos-duration="800"></div>
             </div>
           </div>
 
-          <div v-else class="hidden md:flex justify-between items-center">
+          <div
+            v-else
+            class="hidden md:flex justify-between items-center"
+          >
             <div class="w-1/2 flex justify-end">
               <!-- Bulet hanya muncul di desktop -->
-              <div
-                class="w-4 h-4 bulet rounded-full z-10 hidden md:block"
-                data-aos="fade-in"
-                data-aos-duration="800"
-                data-aos-delay="500"
-              ></div>
+              <div class="w-4 h-4 bulet rounded-full z-10 hidden md:block" data-aos="fade-in" data-aos-duration="800"></div>
             </div>
-            <div class="w-1/2 pl-8 flex justify-start" >
-              <a :href="edu.link" target="_blank" rel="noopener noreferrer" >
-                <div class="card max-w-md" data-aos="fade-left" data-aos-duration="800">
-                  <img :src="edu.logo" class="w-16 h-16 object-contain" />
-                  <div class="text-right">
-                    <p class="text-sm sm:text-xs md:text-sm font-semibold text-yellow-500 mb-1">
-                      {{ edu.period }}
-                    </p>
-                    <h3 class="text-xl sm:text-lg md:text-2xl font-bold text-white mb-1">
-                      {{ edu.institution }}
-                    </h3>
-                    <p class="text-white/80 sm:text-xs font-poppins md:text-sm">{{ edu.major }}</p>
-                  </div>
+            <div class="w-1/2 pl-8 flex justify-start">
+              <a
+                :href="edu.link"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="card max-w-md"
+                data-aos="fade-left"
+              >
+                <img :src="edu.logo" class="w-16 h-16 object-contain" />
+                <div class="text-right">
+                  <p class="text-sm sm:text-xs md:text-sm font-semibold text-yellow-500 mb-1">{{ edu.period }}</p>
+                  <h3 class="text-xl sm:text-lg md:text-2xl font-bold text-white mb-1">{{ edu.institution }}</h3>
+                  <p class="text-white/80 sm:text-xs font-poppins md:text-sm">{{ edu.major }}</p>
                 </div>
               </a>
             </div>
@@ -110,14 +105,8 @@
             >
               <img :src="edu.logo" class="w-16 h-16 object-contain" />
               <div>
-                <p
-                  class="text-sm sm:text-xs md:text-sm font-poppins font-semibold text-yellow-500 mb-1"
-                >
-                  {{ edu.period }}
-                </p>
-                <h3 class="text-xl sm:text-lg md:text-2xl font-bold font-poppins text-white mb-1">
-                  {{ edu.institution }}
-                </h3>
+                <p class="text-sm sm:text-xs md:text-sm font-poppins font-semibold text-yellow-500 mb-1">{{ edu.period }}</p>
+                <h3 class="text-xl sm:text-lg md:text-2xl font-bold font-poppins text-white mb-1">{{ edu.institution }}</h3>
                 <p class="text-white/80 sm:text-xs md:text-sm font-poppins">{{ edu.major }}</p>
               </div>
             </a>
@@ -147,7 +136,7 @@ onMounted(async () => {
 
 <style scoped>
 .edu {
-  background-image: url('../assets/background/wl55.jpg');
+  background-image: url('../assets/background/wl7.jpg');
   background-size: cover;
   background-position: center;
 }
@@ -165,54 +154,11 @@ onMounted(async () => {
   display: flex;
   align-items: center;
   gap: 1rem;
-  transition:
-    transform 0.3s ease,
-    box-shadow 0.3s ease !important;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
 }
 
 .card:hover {
   transform: translateY(-6px) !important;
   box-shadow: 0 12px 32px rgba(0, 0, 0, 0.25) !important;
-}
-
-/* Media Queries untuk Responsivitas */
-@media (max-width: 768px) {
-  .desc {
-    font-size: 19px !important; /* Ukuran font deskripsi lebih kecil pada perangkat mobile */
-    line-height: 29px;
-  }
-  .condes {
-    max-width: 650px;
-  }
-}
-
-@media (max-width: 480px) {
-  .desc {
-    font-size: 13px !important; /* Ukuran font deskripsi lebih kecil pada perangkat mobile kecil */
-    line-height: 19px;
-  }
-  .condes {
-    max-width: 350px;
-    margin-top: -15px;
-  }
-  .card {
-    background-color: rgba(255, 255, 255, 0.08);
-    backdrop-filter: blur(10px);
-    -webkit-backdrop-filter: blur(10px);
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    border-radius: 12px;
-    box-shadow: 0 8px 24px rgba(0, 0, 0, 0.2);
-    padding: 1.3rem;
-    display: flex;
-    align-items: center;
-    gap: 0.8rem;
-    transition:
-      transform 0.3s ease,
-      box-shadow 0.3s ease !important;
-  }
-  .card:hover {
-    transform: translateY(-6px) !important;
-    box-shadow: 0 12px 32px rgba(0, 0, 0, 0.25) !important;
-  }
 }
 </style>
